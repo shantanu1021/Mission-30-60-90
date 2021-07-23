c140332d98be2b832d74d5a73facf04b22918a59
shantanu1021 <shantanudeshpande00@gmail.com>
2021-07-23 22:34:08 +0530

    Apps created

--- mission_30_60_90/mission_30_60_90/settings.py
+++ mission_30_60_90/mission_30_60_90/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mission.apps.MissionConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
