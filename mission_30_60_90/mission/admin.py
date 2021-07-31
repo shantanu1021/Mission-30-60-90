16e941d4b9338153df836119fe77180278fc7f1d
shantanu1021 <shantanudeshpande00@gmail.com>
2021-07-31 21:14:29 +0530

    Models created

--- mission_30_60_90/mission/admin.py
+++ mission_30_60_90/mission/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Missions, Status
 
-# Register your models here.
+admin.site.register(Missions)
+admin.site.register(Status)
+
--- mission_30_60_90/mission/models.py
+++ mission_30_60_90/mission/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import date, datetime
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Missions(models.Model):
+
+    mission_name = models.CharField(max_length=50)
+    no_of_days = models.IntegerField()
+    start_date = models.DateField(default=date.today)
+    aspirant = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    '''
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
+    '''       
+
+class Status(models.Model):
+
+    mission_id = models.ForeignKey(Missions, on_delete=models.CASCADE)
+    day_no = models.IntegerField()
+    is_done = models.BooleanField(default=False)
+    remarks = models.TextField()
+    
